1ca394477b38f70e4371ef294cdb465a1a7527da
Charlie Haley <charlie.haley@hotmail.com>
2022-03-25 22:43:32 +0000

    fix: fetch site id from user (#34)
    * fix: fetch site id from user
    
    * fix: fetch site id from user--amend

--- pkg/api/user.go
+++ pkg/api/user.go
@@ -10,7 +10,7 @@ import (
 )
 
 // there's no nice way of fetching the site ID from the `Viewer` role
-// calling the timeout endpoint seems to return a siteId(?!)
+// calling the user endpoint seems to return a list of sites for the user
 func (c *Client) getSiteId(name string) (*string, error) {
 	loggedIn, err := c.IsLoggedIn()
 	if err != nil {
@@ -24,7 +24,7 @@ func (c *Client) getSiteId(name string) (*string, error) {
 		}
 	}
 
-	url := fmt.Sprintf("%s/%s/api/v2/sites/%s/setting/firewall/timeout", c.Config.String("host"), c.omadaCID, name)
+	url := fmt.Sprintf("%s/%s/api/v2/users/current", c.Config.String("host"), c.omadaCID)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
@@ -42,18 +42,34 @@ func (c *Client) getSiteId(name string) (*string, error) {
 		return nil, err
 	}
 
-	sites := timeoutResponse{}
-	err = json.Unmarshal(body, &sites)
+	user := userResponse{}
+	err = json.Unmarshal(body, &user)
 	if err != nil {
 		return nil, err
 	}
 
-	return &sites.Result.Id, nil
+	for _, s := range user.Result.Privilege.Sites {
+		if s.Key == name {
+			return &s.Value, nil
+		}
+	}
+
+	return nil, fmt.Errorf("failed to find site with name %s", name)
 }
 
-type timeoutResponse struct {
-	Result timeout `json:"result"`
+type userResponse struct {
+	Result user `json:"result"`
 }
-type timeout struct {
-	Id string `json:"siteId"`
+
+type user struct {
+	Privilege privilege `json:"privilege"`
+}
+
+type privilege struct {
+	Sites []site `json:"sites"`
+}
+
+type site struct {
+	Key   string `json:"name"`
+	Value string `json:"key"`
 }
